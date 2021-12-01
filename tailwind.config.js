1241299194c43811d12ada3519140671caafb899
James Moles <jamespmoles@gmail.com>
2021-12-01 13:52:22 -0500

    added social icons, updated name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-    purge: [], // "./*.html"
+    purge: {
+        enabled: true,
+        content: ["./*.html"],
+    }, // "./*.html"
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                twitterblue: "#1d9bf0",
+                linkedinblue: "#0a66c2",
+            },
+        },
     },
     variants: {
         extend: {
